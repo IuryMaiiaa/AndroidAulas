30f544bc41b26b5beff8a6a13e4384704db5e418
Neils01 <iurryy@yahoo.com.br>
2014-02-04 15:25:03 -0300

    adicionando o loop game

--- impossivel/Game.java
+++ impossivel/Game.java
@@ -2,15 +2,31 @@ package com.example.impossivel;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Paint;
 import android.view.Menu;
+import android.view.SurfaceHolder;
 
 public class Game extends Activity {
+	// classe que guarda a logica do jogo
+	Impossivel veiw;
+	
+	// metodo construtor
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		veiw = new Impossivel(this);
+		setContentView(veiw);
+	}
+	
+	// iniciando o loop game
+	protected void onResume() {
+		super.onResume();
+		veiw.resume();
 	}
 
+	
+	// default
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
--- impossivel/Impossivel.java
+++ impossivel/Impossivel.java
@@ -1,5 +1,38 @@
 package com.example.impossivel;
 
-public class Impossivel {
+import android.content.Context;
+import android.graphics.Paint;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class Impossivel extends SurfaceView implements Runnable{
+	// classe para chegar se a surface view pode ser modificada;
+	private SurfaceHolder holder;
+	// classe responsavel por imprimir objetos, letras ,etc na tela
+	private Paint paint;
+	// variavel responsavel pelo loop game
+	private boolean running = false;
+	// thread responsavel pelas interaçoes do usuário
+	Thread renderThread = null;
+
+	public Impossivel(Context context) {
+		super(context);
+		paint = new Paint();
+		holder = getHolder();
+	}
+	
+	public void resume() {
+		running = true;
+		renderThread = new Thread(this);
+		renderThread.start();
+	}
+
+	@Override
+	public void run() {
+		while( running ) {
+			System.out.println("impossivel running");
+		}
+		
+	}
 
 }
