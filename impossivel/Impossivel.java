bd01c90ea7105bd856bae71c3a36fd46c873db2d
Neils01 <iurryy@yahoo.com.br>
2014-02-05 16:47:17 -0300

    adicionado game over

--- impossivel/Impossivel.java
+++ impossivel/Impossivel.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Paint.Style;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
@@ -17,8 +18,12 @@ public class Impossivel extends SurfaceView implements Runnable{
 	// thread responsavel pelas interaçoes do usuário
 	Thread renderThread = null;
 	
-	private int playerY = 300;
+	private int playerX=300 , playerY = 300, playerRaio=50;
+	
+	private int enemyX=100,enemyY=100,enemyRaio = 100;
 
+	private boolean GameOver;
+	
 	public Impossivel(Context context) {
 		super(context);
 		paint = new Paint();
@@ -27,17 +32,40 @@ public class Impossivel extends SurfaceView implements Runnable{
 	
 	public void resume() {
 		running = true;
+		GameOver = false;
 		renderThread = new Thread(this);
 		renderThread.start();
 	}
 	
 	private void drawPlayer(Canvas canvas) {
 		paint.setColor(Color.GREEN);
-		canvas.drawCircle(100,playerY,100,paint);
+		canvas.drawCircle(playerX,playerY,playerRaio,paint);
+	}
+	
+	private void drawEnemy(Canvas canvas) {
+		paint.setColor(Color.RED);
+		enemyRaio++;
+		canvas.drawCircle(enemyX, enemyY, enemyRaio, paint);
 	}
 	
 	public void moveDonw(int pixels) {
 		playerY += pixels;
+	}
+	
+	private void checkCollision(Canvas canvas) {
+		double distancia = Math.pow(playerY-enemyY, 2) + Math.pow(playerX-enemyX, 2);
+		distancia = Math.sqrt(distancia);
+		
+		if (distancia <= playerRaio + enemyRaio) {
+			GameOver = true;
+		}
+	}
+	
+	private void stopGame(Canvas canvas) {
+		paint.setStyle(Style.FILL);
+		paint.setColor(Color.LTGRAY);
+		paint.setTextSize(100);
+		canvas.drawText("GAME OVER",50,150,paint);
 	}
 
 	@Override
@@ -48,7 +76,17 @@ public class Impossivel extends SurfaceView implements Runnable{
 			}
 			Canvas canvas = holder.lockCanvas();
 		
+			canvas.drawColor(Color.BLACK);
+			
+			drawEnemy(canvas);
 			drawPlayer(canvas);
+			
+			checkCollision(canvas);
+			
+			if (GameOver) {
+				stopGame(canvas);
+				break;
+			}
 			
 			holder.unlockCanvasAndPost(canvas);
 		}
